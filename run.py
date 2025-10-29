1c80148e119da331fab6f631af27359a3f7ea489
Michael Benjamin Quaye <mikequaye2006@gmail.com>
2025-10-29 15:57:11 +0000

    Load port from environment variable in run.py
    Updated the run.py to load the port from environment variables.

--- run.py
+++ run.py
@@ -1,13 +1,16 @@
 from app import create_app, db
 from app.models import User, Product, Payment, Order, CartItem, Category
 from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = create_app()
 
+# Run only in development mode
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-    load_dotenv()
-        
-    app.run(debug=True, host='0.0.0.0', port=7000)
+    port = int(os.getenv("PORT", 7000))  # Render assigns a PORT dynamically
+    app.run(debug=True, host='0.0.0.0', port=port)
